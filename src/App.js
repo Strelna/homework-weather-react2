7fe01f9bb4e15a920768ed158b38cb50147247bb
Strelna <aleesartshop@gmail.com>
2023-02-21 20:06:27 +0200

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather app
+     <h1>Weather app</h1>
+     <footer>
+This project is created by <a href='https://www.linkedin.com/in/olena-dudnyk/'>Dudnyk Olena</a> and it's{" "}
+     <a href='https://github.com/Strelna/homework-weather-react2' target="_blank">open-sourced on GitHub</a>
+     </footer>
     </div>
   );
 }
