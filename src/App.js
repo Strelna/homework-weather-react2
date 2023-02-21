641d483da2fe931db2bf7962dfed86361c78199e
Strelna <aleesartshop@gmail.com>
2023-02-21 20:26:13 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className='container'>
      <h1>Weather app</h1>
      <footer>
-This project is created by <a href='https://www.linkedin.com/in/olena-dudnyk/'>Dudnyk Olena</a> and it's{" "}
+This project is created by <a href='https://www.linkedin.com/in/olena-dudnyk/' target='_blank' rel="noreferrer">Dudnyk Olena</a> and it's{" "}
      <a href='https://github.com/Strelna/homework-weather-react2' target='_blank' rel="noreferrer">open-sourced on GitHub</a>
      </footer>
-    </div>
+    </div></div>
   );
 }
 
