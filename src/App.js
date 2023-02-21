57df482200d5b017c24de66ba3891842277d9494
Strelna <aleesartshop@gmail.com>
2023-02-21 20:09:51 +0200

    Added noreferrer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ export default function App() {
      <h1>Weather app</h1>
      <footer>
 This project is created by <a href='https://www.linkedin.com/in/olena-dudnyk/'>Dudnyk Olena</a> and it's{" "}
-     <a href='https://github.com/Strelna/homework-weather-react2' target="_blank">open-sourced on GitHub</a>
+     <a href='https://github.com/Strelna/homework-weather-react2' target='_blank' rel="noreferrer">open-sourced on GitHub</a>
      </footer>
     </div>
   );
