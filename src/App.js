1e6304d95bf7275d81d428279cec791d40b53f15
Strelna <aleesartshop@gmail.com>
2023-02-22 11:45:19 +0200

    Added Weather

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Weather from "./Weather"
 import './App.css';
 
 export default function App() {
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <div className='container'>
      <h1>Weather app</h1>
+     <Weather />
      <footer>
 This project is created by <a href='https://www.linkedin.com/in/olena-dudnyk/' target='_blank' rel="noreferrer">Dudnyk Olena</a> and it's{" "}
      <a href='https://github.com/Strelna/homework-weather-react2' target='_blank' rel="noreferrer">open-sourced on GitHub</a>
