b0a05eaff2c46833169784e9c35c6f2c3fb7186f
Strelna <aleesartshop@gmail.com>
2023-02-28 23:49:45 +0200

    Added WeatherForecast component

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -56,8 +57,8 @@ if (weatherData.ready) {
                 </div>
             </form>
             <WeatherInfo  data={weatherData}/>
-           
-            </div>
+           <WeatherForecast />
+        </div>
     );
 } else { 
     search();
