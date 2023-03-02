6a2662900e888161ac7236192f1a2d29309b2cb7
Strelna <aleesartshop@gmail.com>
2023-03-02 18:12:15 +0200

    Added WeatherForecastDay component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,31 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
-import "./WeatherForecast.css"
+import "./WeatherForecast.css";
 import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 export default function WeatherForecast(props) {
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
+
 function handleResponse(response) {
-console.log(response.data);
+setForecast(response.data.daily);
+setLoaded(true);
 }
-    let apiKey =  "f74e0a84t57a0fa0bdo4a8be9643e330";
+    
+    if (loaded) {
+        console.log(forecast);
+      return (
+            <div className="WeatherForecast">
+                <div className="row">
+                    <div className="col">
+                    <WeatherForecastDay data={forecast[0]} />
+                    
+                </div>
+            </div>
+            </div>
+        );
+    
+    } else {
+     let apiKey =  "f74e0a84t57a0fa0bdo4a8be9643e330";
     let longitude = props.coordinates.longitude;
     let latitude = props.coordinates.latitude;
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lat=${latitude}&lon=${longitude}&key=${apiKey}&units=metric`
     
     axios.get(apiUrl).then(handleResponse);
-    return (
-        <div className="WeatherForecast">
-            <div className="row">
-                <div className="col">
-                <div className="WeatherForecast-day">Thu</div>
-                <WeatherIcon code="clear-sky-day" size={36} />
-                <div className="WeatherForecast-temperatures">
-                    <span className="WeatherForecast-temperature-max">19°</span>
-                    <span className="WeatherForecast-temperature-min">10°</span>
-                </div>
-                </div>
-                
-            </div>
-        </div>
-    )
+    return null; 
+    }
+   
 }
+
