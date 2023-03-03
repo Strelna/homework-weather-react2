28aaad51ef0074978d7ab688083b3f57b41592b2
Strelna <aleesartshop@gmail.com>
2023-03-03 23:53:51 +0200

    added daily forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -19,14 +19,18 @@ setLoaded(true);
       return (
             <div className="WeatherForecast">
                 <div className="row">
-                    <div className="col">
-                    <WeatherForecastDay data={forecast[0]} />
-                    
+                    {forecast.map(function (dailyForecast, index) {
+                        if (index < 5) {
+                           return (
+                         <div className="col" key={index}>
+                    <WeatherForecastDay data={dailyForecast} />
+                    </div>
+                    ); 
+                    }
+                    })}
                 </div>
             </div>
-            </div>
         );
-    
     } else {
      let apiKey =  "f74e0a84t57a0fa0bdo4a8be9643e330";
     let longitude = props.coordinates.longitude;
